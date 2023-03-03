bb1d2d41453ab7d4a8273d392166196f19aa6124
onomlight <pass041@nate.com>
2023-03-03 17:35:48 +0900

    화살표 예시 추가 및 내용 추가 수정

--- hover/js/hover.js
+++ hover/js/hover.js
@@ -1,23 +1,8 @@
 $(function () {
-
-    /* ---- ---- ---- ----HEADER---- ---- ---- ---- */
-   
-
-    $(".header__nav nav .depth1 > a").on("mouseenter focus", function () {
-        $(".header__nav nav .depth2_wrap").hide();
-        $(this).next(".depth2_wrap").show();
-        $(".header__nav nav .depth1").removeClass("on");
-        $(this).parent('li').addClass('on');
-        $("#header").addClass("on");
-    });
-    $(".header__nav nav").mouseleave(function () {
-        $(".header__nav nav .depth2_wrap").hide();
-        $("#header").removeClass("on");
-        $(".header__nav nav .depth1").removeClass("on");
-    });
-    
-
-
+  $(".headernav ").on("mouseenter", function () {
+    $("#header").addClass("on");
+  });
+  $(".headernav ").mouseleave(function () {
+    $("#header").removeClass("on");
+  });
 });
-
-
