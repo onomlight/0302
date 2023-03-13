f2b3a70e7f6aa840a9bd04ea91da1b44c725fa36
onomlight <pass041@nate.com>
2023-03-13 15:26:55 +0900

    경로 작업 재설정

--- hover/js/login.js
+++ hover/js/login.js
@@ -10,3 +10,56 @@ $(document).ready(function () {
     }
   });
 });
+
+function lchk() {
+  if (!mform.id.value) {
+    alert("아이디를 입력하세요");
+    mform.id.focus();
+    return;
+  }
+  if (!mform.pw.value) {
+    alert("비밀번호를 입력하세요");
+    mform.pw.focus();
+    return;
+  }
+  mform.submit();
+}
+function mchk() {
+  if (!sform.name.value) {
+    alert("이름을 입력하세요");
+    sform.name.focus();
+    return;
+  }
+  if (!sform.id.value) {
+    alert("아이디를 입력하세요");
+    sform.id.focus();
+    return;
+  }
+  if (!sform.pw.value) {
+    alert("비밀번호를 입력하세요");
+    sform.pw.focus();
+    return;
+  }
+  if (!sform.pw1.value) {
+    alert("비밀번호확인을 입력하세요");
+    sform.pw1.focus();
+    return;
+  }
+  if (sform.pw.value != sform.pw1.value) {
+    alert("비밀번호를 잘못입력했습니다. 다시 입력해 주세요.");
+    sform.pw.value = "";
+    sform.pw1.value = "";
+    return;
+  }
+  if (!sform.hu.value) {
+    alert("휴대폰번호를 입력하세요");
+    sform.hu.focus();
+    return;
+  }
+  if (!sform.birth.value) {
+    alert("생년월일을 입력하세요");
+    sform.birth.focus();
+    return;
+  }
+  sform.submit();
+}
